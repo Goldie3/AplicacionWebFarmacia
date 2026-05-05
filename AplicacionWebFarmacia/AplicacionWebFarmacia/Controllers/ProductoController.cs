554b26f0321c8f35966e8b360e9eaa2d2df7cfbd
Goldie3 <brunoaltamirano978@gmail.com>
2026-05-05 17:37:11 -0400

    agregado base

---
+++ AplicacionWebFarmacia/AplicacionWebFarmacia/Controllers/ProductoController.cs
@@ -0,0 +1,18 @@
+﻿using AplicacionWebFarmacia.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace AplicacionWebFarmacia.Controllers
+{
+    public class ProductoController : Controller
+    {
+        List<Producto> listaProductos = new List<Producto>() {
+            new Producto(1, "Jarabe", DateTime.Parse("2026-04-30"),20,5000),
+            new Producto(2, "Paracetamol", DateTime.Parse("2026-02-02"),50,2000)
+            };
+        public IActionResult Index()
+        {
+            return View(listaProductos);
+        }
+    }
+}
---
+++ AplicacionWebFarmacia/AplicacionWebFarmacia/Models/Producto.cs
@@ -0,0 +1,20 @@
+﻿namespace AplicacionWebFarmacia.Models
+{
+    public class Producto
+    {
+        public int idProducto { get; set; }
+        public string nombreProducto { get; set; }
+        public DateTime fechaIngreso { get; set; }
+        public int cantidadProducto { get; set; }
+        public int precio { get; set; }
+    public Producto(int idProducto, string nombreProducto, DateTime fechaIngreso, int cantidadProducto, int precio)
+        {
+            this.idProducto = idProducto;
+            this.nombreProducto = nombreProducto;
+            this.fechaIngreso = fechaIngreso;
+            this.cantidadProducto = cantidadProducto;
+            this.precio = precio;
+        }
+        
+    }
+}
